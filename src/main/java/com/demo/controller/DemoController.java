8a04fb2e52e0dae2489ed9b4b98161f6f2651ea9
Max Qiu <yueyiqiu178@gmail.com>
2024-02-23 18:18:15 +0800

    first commit

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+	
+	@RequestMapping(value="/hello",method= RequestMethod.GET)
+	public String demo() {
+		return "Hello";
+	}
+	
+}
---
+++ src/main/java/com/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,10 @@
+package com.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.service.DemoService;
+
+@Service
+public class DemoServiceImpl implements DemoService{
+
+}
